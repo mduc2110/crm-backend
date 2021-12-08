c0461cf670d6e446abe581dce0903df6b2277590
Andres <vmd211099@gmail.com>
2021-12-08 17:14:41 +0700

    setup eslint, babel and test api

---
+++ .eslintrc.js
@@ -0,0 +1,12 @@
+export default {
+   env: {
+      browser: true,
+      es2021: true,
+   },
+   extends: "eslint:recommended",
+   parserOptions: {
+      ecmaVersion: 13,
+      sourceType: "module",
+   },
+   rules: {},
+};
