fda56a4eb5a7cd97314fbba3ff6dbbfbdf133855
Duc <vmd211099@gmail.com>
2021-12-14 07:46:30 +0700

    folder structure

---
+++ controllers/user.controller.js
@@ -0,0 +1,5 @@
+export const userController = {
+   addUser: (req, res) => {
+      return res.json("OK");
+   },
+};
--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,5 @@
 import express from "express";
-import v1RouterUser from "./v1/users.routes";
+import v1RouterUser from "./v1/user.routes";
 const route = express.Router();
 
 route.use("/v1", v1RouterUser);
